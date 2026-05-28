64126596b9054cee29912fa64d65a37aa272d2c0
Jesse Owen <92907818+Jowen6500@users.noreply.github.com>
2026-05-28 20:43:18 +0700

    Setup & Done 2.1 Interface Chapter
    Imported Assets from Chapter 2.3
    Added "Assets\Game\Resources" to gitignore(Asset's too Big to upload to git)

---
+++ Assets/Game/Scripts/TestScript/TestInterface/TestInterface.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class TestInterface : MonoBehaviour
+{
+    //interface example
+    //Only declare function, no block of code, no var also.
+    //in interface, function is abstract and public
+    //interface will be used only by other classes, using its abstract function and properties
+    public interface IInteractable//creating interface Interactable
+    {
+        public string Name {get;}//creating its properties
+        public void Interact();//creating its function/method
+    }
+
+    public class Door : IInteractable
+    {
+        [SerializeField] protected string _name;//var for property's name
+        public string Name => _name;//this means get "_name" value and assign it into the "Name" property inside the IInteractable
+
+        public void Interact()
+        {
+            //type block of codes here
+        }
+    }
+    //in inheritance, child class can only have one parent. interfaces enables child class to have more that one parents.
+}
