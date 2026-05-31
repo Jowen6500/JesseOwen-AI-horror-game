19b76e4d19d3a744ed53a77f2ae6eabab8198e90
Jesse Owen <92907818+Jowen6500@users.noreply.github.com>
2026-05-31 20:04:26 +0700

    Done 2.5 Item Implementation

--- Assets/Game/Scripts/Interactable/Pickable/Item.cs
+++ Assets/Game/Scripts/Interactable/Pickable/Item.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 //item script
 public class Item : MonoBehaviour, IInteractable, IPickable
 {
     [SerializeField] private ItemData _itemData;//var to determine item data
     public string Name => _itemData.Name;//get "_itemData.Name" value and assign it into the "Name" property inside the IInteractable
-    
-    public void Interact()//Interact method
-    {
-        Pickup();
-    }
+    public UnityEvent OnItemPicked;//unity event var, used to tell other module/class that there's an item being picked up
 
     public void Pickup()//Pickup Method
     {
-        throw new System.NotImplementedException();
+        OnItemPicked?.Invoke();//invoke "OnItemPicked" to call pickup item event
+        Destroy(gameObject);//destroy its game object(after the code above)
+    }
+    
+    //created context menu so the interact function can be called through the door component inside the inspector
+    [ContextMenu("Interact Item")]
+    public void Interact()//Interact method
+    {
+        Pickup();
     }
 }
